a8523b618d59a106d86adbdc08a3197a4c0aaa5c
JovicAbengona <jvlabengona@gmail.com>
2022-06-27 13:18:20 +0800

    Minor changes

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/api/get_movies', (req, res) => {
 app.get('/api/get_reviews', (req, res) => {
     db.query(`SELECT
             movies.id AS movie_id, movies.name AS movie_name,
-            movie_reviews.id AS movie_review_id, CONCAT(movie_reviews.first_name, ' ', movie_reviews.last_name) AS full_name, movie_reviews.email_address, movie_reviews.review, movie_reviews.rating
+            movie_reviews.id AS movie_review_id, CONCAT(movie_reviews.first_name, ' ', movie_reviews.last_name) AS full_name, movie_reviews.email_address, movie_reviews.review, movie_reviews.rating, movie_reviews.created_at
         FROM movie_reviews
         INNER JOIN movies ON movies.id = movie_reviews.movie_id
         ORDER BY movie_review_id DESC;`, 
@@ -36,7 +36,9 @@ app.post('/api/create_rating', (req, res) => {
     const form_data = req.body.form_data;
 
     db.query(`INSERT INTO movie_reviews (movie_id, first_name, last_name, email_address, review, rating, created_at, updated_at)
-        VALUES (?, ?, ?, ?, ?, ?, NOW(), NOW());`, [form_data.movie_id, form_data.first_name, form_data.last_name, form_data.email_address, form_data.review, form_data.rating], (err, result) => {
+        VALUES (?, ?, ?, ?, ?, ?, NOW(), NOW());`, 
+        [form_data.movie_id, form_data.first_name, form_data.last_name, form_data.email_address, form_data.review, form_data.rating], 
+        (err, result) => {
             res.send(result);
         })
 });
